51acb34a9423bfc0c9747e6637ebbbcc543ef7af
bin <dangngocbin2000@gmail.com>
2022-12-12 16:12:45 +0700

    [add]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Home } from "./components/Home";
+import { Employee } from "./components/Employee";
+import { Department } from "./components/Department";
+import { Navigation } from "./components/Navigation";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Navigation></Navigation>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/employee" element={<Employee />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+
+export class Navigation extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+          <Nav className="me-auto">
+            <NavLink to="/home" className="d-inline p-2 bg-dark text-white">
+              Home
+            </NavLink>
+            <NavLink to="/employee" className="d-inline p-2 bg-dark text-white">
+              Employee
+            </NavLink>
+            <NavLink
+              to="/department"
+              className="d-inline p-2 bg-dark text-white"
+            >
+              Department
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  }
+}
